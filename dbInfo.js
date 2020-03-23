7ba38e766541a4d81401d2a48b6f5c952db8e10e
Tyler Knapp <knapptr@gmail.com>
2020-03-23 15:31:55 -0700

    finalize deployment

--- dbInfo.js
+++
@@ -1,7 +0,0 @@
-
-const PW = "Relyt352%21" //URL ENCODED: ORIGINAL IS "Relyt352!"
-const dev_db_url= `mongodb+srv://Knapptr:${PW}@cluster0-m6riz.gcp.mongodb.net/test?retryWrites=true&w=majority`;
-const dbConnect = process.env.MONGODB_URI || dev_db_url;
-
-module.exports = dbConnect
-
